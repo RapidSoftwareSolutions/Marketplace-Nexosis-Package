0ee54b9ddb4a1a345e79af8dfa627c11ba70e7fc
ivz-dev <zakutynsky@yahoo.com>
2017-08-07 15:17:59 +0300

    fixed bug'

--- src/routes/deleteDataFromDataset.php
+++ src/routes/deleteDataFromDataset.php
@@ -33,7 +33,7 @@ $app->post('/api/Nexosis/deleteDataFromDataset', function ($request, $response) 
             $result['callback'] = 'success';
             $result['contextWrites']['to'] = is_array($responseBody) ? $responseBody : json_decode($responseBody);
             if(empty($result['contextWrites']['to'])) {
-                $result['contextWrites']['to']['status_msg'] = "Api return no results";
+                $result['contextWrites']['to']['status_msg'] = "Data successfully deleted!";
             }
         } else {
             $result['callback'] = 'error';
--- src/routes/deleteSession.php
+++ src/routes/deleteSession.php
@@ -33,7 +33,7 @@ $app->post('/api/Nexosis/deleteSession', function ($request, $response) {
             $result['callback'] = 'success';
             $result['contextWrites']['to'] = is_array($responseBody) ? $responseBody : json_decode($responseBody);
             if(empty($result['contextWrites']['to'])) {
-                $result['contextWrites']['to']['status_msg'] = "Api return no results";
+                $result['contextWrites']['to']['status_msg'] = "Session successfully deleted!";
             }
         } else {
             $result['callback'] = 'error';
